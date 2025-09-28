2103970d2f45e88527a7de093cb38e0517ebf625
aymantarek16 <aymantarekm16@gmail.com>
2025-09-28 14:19:21 +0300

    change styles of site to modern styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,48 @@ import Main from "./components/3-main/Main";
 import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/Footer";
 import Header from "./components/1-header/Header";
-// import Header from "./components/1-header/Header";
 
 function App() {
   const [showScrollBtn, setshowScrollBtn] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.scrollY > 300 ? setshowScrollBtn(true) : setshowScrollBtn(false);
-    });
+    const handleScroll = () => {
+      setshowScrollBtn(window.scrollY > 300);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", 
+    });
+  };
+
   return (
-    <div id="up" className="container">
+    <div className="container">
       <Header />
-      <Hero  />
-      <div className="divider"/>
+      <Hero />
+      <div className="divider" />
       <Main />
-      <div className="divider"  />
+      <div className="divider" />
       <Contact />
       <div className="divider" />
       <Footer />
 
-      <a href="#up">
+      {showScrollBtn && (
         <button
+          onClick={scrollToTop}
           className="icon-keyboard_arrow_up scroll2Top"
           style={{
-            cursor: showScrollBtn ? "pointer" : "default",
             opacity: showScrollBtn ? "1" : "0",
             transition: ".8s",
           }}
         ></button>
-      </a>
+      )}
     </div>
   );
 }
